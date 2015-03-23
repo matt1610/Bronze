74711caacb3aea6a4896eb8389a7c7dae999725c
matt1610 <mattstarkey@me.com>
2015-03-23 22:04:29 +0200

    7
    7

--- index.js
+++ index.js
@@ -42,56 +42,21 @@ app.use('/', express.static(path.join(__dirname, 'stream')));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-// SOCKET LIST
-var sockets = {};
-
-// Start();
-// SOCKET CONNECTION
-io.on('connection', function(socket) {
- 
-  sockets[socket.id] = socket;
-  console.log("Total clients connected : ", Object.keys(sockets).length);
- 
-  socket.on('disconnect', function() {
-    delete sockets[socket.id];
- 
-    // no more sockets, kill the stream
-    if (Object.keys(sockets).length == 0) {
-      app.set('watchingFile', false);
-      if (proc) proc.kill();
-      fs.unwatchFile('./stream/image_stream.jpg');
-    }
-  });
- 
-  socket.on('start-stream', function() {
-    startStreaming(io);
-  });
- 
-});
  
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
-
-
-
-
-
-
  
 function CheckToStop() {
-
   if (!ON) {
     app.set('watchingFile', false);
     if (proc) proc.kill();
     fs.unwatchFile('./stream/image_stream.jpg');
     console.log('Stopped');
   }
-
-  // if (Object.keys(sockets).length == 0) {
-  // }
 }
+
+
 
 // Get Data URI of Image
 function base64Image(src) {
@@ -102,18 +67,9 @@ function base64Image(src) {
 
 
 
-
-
-
  
 function startStreaming(io) {
- 
-  if (app.get('watchingFile')) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    return;
-  }
- 
-  // var freq = (5 * 1000).toString();
+
   var args = ["-w", "900", "-h", "675", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "30000"];
   proc = spawn('raspistill', args);
  
@@ -122,21 +78,19 @@ function startStreaming(io) {
   app.set('watchingFile', true);
  
   fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-
     var dataUri = base64Image("./stream/image_stream.jpg");
-
-    console.log('Done');
 
     CheckToStop();
 
-    myFirebaseRef.child('img').set(dataUri);
+    myFirebaseRef.child('img').set(dataUri, function(error) {
+        if (error) {
+          console.log(error);
+        } else{
+          console.log('Image Uploaded');
+        }
+    });
 
-    // myFirebaseRef.set({
-    //   img : dataUri
-    // });
-
-  })
+  }); // End watchFile
  
 } // End Streaming Fn
 
